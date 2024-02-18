290386c6ae08a2fca996129a459354e5ee1e5c03
MiguelMoreiraDev <contatomigueldev@gmail.com>
2024-02-18 16:18:15 -0300

    animação js

--- js/script.js
+++ js/script.js
@@ -24,3 +24,48 @@ function ativarProduto(parametro) {
 }
 
 parametros.forEach(ativarProduto);
+
+//Perguntas
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function eventoPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  pergunta.setAttribute("aria-expanded", "true");
+  const ativa = resposta.classList.contains("ativa");
+  resposta.classList.toggle("ativa");
+}
+
+function ativarPergunta(pergunta) {
+  pergunta.addEventListener("click", eventoPergunta);
+}
+
+perguntas.forEach(ativarPergunta);
+
+//Galeria de Bicicletas
+
+const galeria = document.querySelectorAll(".bicicletas-imagem img");
+const galeriaContainer = document.querySelector(".bicicletas-imagem");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+// Animação
+
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
